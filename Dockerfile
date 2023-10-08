d3a07d6b0ec5c724a5c9edb473e5b45a44c9c5d8
ya-ne-kit <120.23@mail.ru>
2023-10-08 15:39:27 +0300

    done

--- Dockerfile
+++ Dockerfile
@@ -1,2 +1,8 @@
 FROM ubuntu
-RUN echo "hello mir"
+RUN apt-get update && apt-get install -y \
+    default-jre \
+    default-jdk
+COPY ./out/artifacts/my_app_jar/*.jar /app/app.jar
+WORKDIR /app
+
+CMD ["java", "-jar", "app.jar"]
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,12 @@
 import java.util.Scanner;
 
-
-
 public class Main {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int num1 = in.nextInt();
-        int num2 = in.nextInt();
-        System.out.println(num1+num2);
+        while (true) {
+            Scanner in = new Scanner(System.in);
+            int num1 = in.nextInt();
+            int num2 = in.nextInt();
+            if (num1 + num2 == 0) break; else System.out.println(num1 + num2);
+        }
     }
 }
